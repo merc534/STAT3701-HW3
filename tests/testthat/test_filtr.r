541c239ab1cf68b21407454b4a69f8e71fc57807
merc534 <mercurioryan53@yahoo.com>
2018-03-05 10:45:35 -0600

    fixed data4 and test for filtr

--- tests/testthat/test_filtr.r
+++ tests/testthat/test_filtr.r
@@ -1,10 +1,10 @@
 
            
-context("Filtering out one column")           
+context("filtr")           
 test_that("filtr is a wrapper for dplyr::select()",{          
            
 x = filtr(data1, 1)
-y = select(data1, 1)
+y = dplyr::select(data1, 1)
            
 expect_identical(x,y)           
            
