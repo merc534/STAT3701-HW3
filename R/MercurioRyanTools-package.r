e9ee824dad5159c7b57f44d29589609901827a96
merc534 <mercurioryan53@yahoo.com>
2018-03-05 13:27:10 -0600

    package documentation

--- R/MercurioRyanTools-package.r
+++ R/MercurioRyanTools-package.r
@@ -1,5 +1,10 @@
 #' MercurioRyanTools.
 #'
+#' 3/7/2018
+#' This package is being submited as HW#3 for STAT 3701 at UMN.
+#' 
+#' I have not included the Quiz/HW functions that I have deemed redundant. 
+#'
 #' @name MercurioRyanTools
 #' @docType package
 NULL
